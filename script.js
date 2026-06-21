3492ebb2c2a9784712a20fa949d7e353c6e82589
cole-g-123 <24316cg@hvhs.school.nz>
2026-06-21 22:18:12 +0000

    Jun 21, 2026, 10:18 PM

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-console.log("Hello world!")
+console.log("Cafe Ordering System");
+
+function submitOrder() {
+    const name = document.getElementById("name").value;
+    const order = document.getElementById("order").value;
+    const quantity = document.getElementById("quantity").value;
+}
 
